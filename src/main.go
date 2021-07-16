f10482a65d36a5e129b3d57ccb301bed57618a6f
Alexis Hope <alexis@cvofhope.com>
2021-07-16 10:57:17 +1000

    feat: connect to sanity and run query

--- src/main.go
+++ src/main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
+	sanity "github.com/sanity-io/client-go"
 )
 
 func main() {
 
-	envFile := flag.String("e", ".env", "optionally specify an env file with `=example.env`")
+	envFile := flag.String("e", ".env", "optionally specify an env file with `example.env`")
 
 	err := godotenv.Load(*envFile)
-
 	if err != nil {
 		log.Fatalf("No env file found")
 	}
+
+	projectId := os.Getenv("SANITY_STUDIO_PROJECT_ID")
+	dataset := os.Getenv("SANITY_STUDIO_API_DATASET")
+	token := os.Getenv("SANITY_STUDIO_TOKEN")
+
+	client, err := sanity.New(projectId,
+		sanity.WithCallbacks(sanity.Callbacks{
+			OnQueryResult: func(result *sanity.QueryResult) {
+				log.Printf("Sanity queried in %d ms!", result.Time)
+			},
+		}),
+		sanity.WithToken(token),
+		sanity.WithDataset(dataset))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Connected to project:%s dataset:%s\n", projectId, dataset)
+
+	rawQuery := `
+		*[]{
+			_id,
+			body[_type match $type]
+		}
+	`
+	query := client.Query(rawQuery)
+
+	result, err := query.Do(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Time %d \n", result.Time)
+	bytes, err := result.Result.MarshalJSON()
+	fmt.Printf("Result %s \n", string(bytes))
 }
